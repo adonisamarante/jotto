9b9912c62b4c285c829a91646092055df25ec842
Adonis Amarante <adonis.amarante@kiper.com.br>
2020-09-13 20:21:06 -0300

    added tests for propTypes

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,14 +2,21 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
-const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// little danger: if you change default props, you might get passing tests that
+// are actually failing in production. Be careful.
+const defaultProps = {
+  success: false,
 };
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
+};
 
 test("renders without error", () => {
   const wrapper = setup();
@@ -27,4 +34,8 @@ test("renders non-empty congrats message when `success` prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps); // if it is undefined, it means the propTypes are all good
 });
