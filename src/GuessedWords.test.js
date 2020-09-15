6aad41f4f736e7ff777c5dab97945bf24105e223
Adonis Amarante <adonis.amarante@kiper.com.br>
2020-09-15 00:09:15 -0300

    guessedWords component and first tests

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import GuessedWords from "./GuessedWords";
+
+const defaultProps = {
+  guessedWords: [
+    {
+      guessedWord: "train",
+      letterMatchCount: 3,
+    },
+  ],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+// `describe` is a way of grouping tests, for different contexts
+describe("if there are no words guessed", () => {
+  // declaring here to be accessible in the `beforeEach` and the other tests inside this `describe`
+  let wrapper;
+  // this will run before each test inside this `describe`, in this case, setup() will be used in all tests
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+describe("if there are words guessed", () => {});
