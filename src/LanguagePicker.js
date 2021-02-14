d84c4c39732a6b3ad46179ca38751da2523c58fa
Adonis Amarante <adonis.amarante@kiper.com.br>
2021-02-14 00:47:43 -0300

    strings context and language picker

---
+++ src/LanguagePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function LanguagePicker({ setLanguage }) {
+  return <div data-test='component-language-picker'></div>;
+}
+
+LanguagePicker.propTypes = {
+  setLanguage: propTypes.func.isRequired,
+};
+
+export default LanguagePicker;
